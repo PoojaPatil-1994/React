65592f583c0e82b6986d63f368b91135a521f2bc
PoojaPatil-1994 <poojamane2294@gmail.com>
2023-12-17 13:34:45 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/comon/navbar/Navbar';
-import Card from './component/comon/card/Card';              
+import Card from './component/comon/card/Card';
 import Banner from './component/comon/banner/Banner';
 import Addition from './component/comon/addition/Addition';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Banner/>
-      <Card/>
-      <Card/>
-      <Addition/>
-      
+      <BrowserRouter >
+      <Navbar />
+        <Routes>
+          <Route path="/" element={ <Banner />} /> {/* 👈 Renders at /app/ */}
+          <Route path="/about" element={  <Card />} /> {/* 👈 Renders at /app/ */}
+          <Route path="/table" element={ <Addition />} /> {/* 👈 Renders at /app/ */}
+        </Routes>
+      </BrowserRouter>
+     
+     
+     
+    
     </div>
   );
 }
--- src/component/comon/addition/Addition.jsx
+++ src/component/comon/addition/Addition.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 import { useState } from "react";
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 
 export default function Addition() {
     const [count, setCount] = useState(0);
     const [apiData, setApiData] = useState([]);
+    // const [addApiData, setAddApiData] = useState([]);
     useEffect(() => {
         setCount(5)
         getApi()
+       
     }, []);
 
     const getApi = async () => {
@@ -20,10 +22,16 @@ export default function Addition() {
             // }
         });
         setApiData(getData.data)
-
     }
-    console.log(apiData)
+    console.log(apiData);
 
+    // const addApi = async  () => {
+    //     const addData = await axios ({
+    //         method: 'post',
+    //         url: 'https://crud-omega-sooty.vercel.app//add' ,
+    //     });
+    //     setAddApiData(addData.data)
+    // }
 
     return (
         <div>
@@ -42,7 +50,38 @@ export default function Addition() {
                 </div>
             </div>
 
-            <table className='table table-bordered'>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Add New
+            </button>
+
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div className="row">
+                                <div className="col-6">
+                                    <input type="text" name="" id="" placeholder='Tital' className=' form-control' />
+                                </div>
+                                <div className="col-6">
+                                    <input type="text" name="" id="" placeholder='Url' className=' form-control' />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-success">Save </button>
+                            <button type="button" class="btn btn-primary">Update </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <table className='table table-bordered mt-3'>
                 <thead>
                     <tr >
                         <th>Sr No.</th>
@@ -50,27 +89,27 @@ export default function Addition() {
                         <th>Title</th>
                         <th>Url</th>
                         <th>Action</th>
-
                     </tr>
-
                 </thead>
                 <tbody>
-                    
-                        {
-                            apiData.map((item , index) => {
-                                return (
-                                    <tr >
-                                        <td>{index+1}</td>
-                                        <td>{item.id}</td>
-                                        <td>{item.title}</td>
-                                        <td>{item.url}</td>
-                                        <td><button className='btn btn-primary' onClick={()=>alert(item.title)}>click</button></td>
-                                       
-                                    </tr>
-                                )
-                            })
-                        }
-                    
+                    {
+                        apiData.map((item, index) => {
+                            return (
+                                <tr >
+                                    <td>{index + 1}</td>
+                                    <td>{item.id}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.url}</td>
+                                    <td>
+                                    <button   type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+                                    <button className='btn btn-danger ms-2' >Delete</button>
+                                    </td>
+
+                                </tr>
+                            )
+                        })
+                    }
+
                 </tbody>
             </table>
 
